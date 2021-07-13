35e4a44548553bd836865de5903e09f07d83da27
Danny Lin <dannyjabez@gmail.com>
2021-07-13 10:30:25 -0400

    react hook 119

--- README.md
+++ README.md
@@ -14,12 +14,21 @@
                 (2).如果新狀態依賴於原狀態 ===> 使用函數方式
                 (3).如果需要在setState()執行後獲取最新的狀態數據,要在第二個callback函數中讀取
 
-
-
 ## 2.lazyLoad
+    路由組件的lazyLoad
+        //1.通過React的lazy函數配合import()函數動態加載路由組件 ===> 路由組件代碼會被分開打包
+        const Login = lazy(()=>import('@/pages/Login'))
 
+        //2.通過<Suspense>指定在加載得到路由打包文件前顯示一個自訂義loading介面
+        <Suspense fallback={<h1>loading...</h1>}>
+            <Switch>
+                <Route path="/xxx" component={Xxxx} />
+                <Redirect to="/login"/>
+            </Switch>
+        </Suspense>
 
 ## 3.Hooks
+
     1.React Hook/Hooks是甚麼?
         (1) Hook是React 16.8.0版本增加的新特性/新語法
         (2) 可以讓你在函數組件中使用state以及其他的React特性
@@ -38,3 +47,26 @@
         (4) setXxx()2種寫法:
             setXxx(newValue): 參數為非函數值, 直接指定新的狀態值, 內部用其覆蓋原來的狀態值
             setXxx(value => newValue): 參數為函數, 接收原本的狀態值, 返回新的狀態值, 內部用其覆蓋原來的狀態值
+
+## 4.Effect Hook
+    (1) Effect Hook 可以讓你在函數組件中執行副作用操作(用於模擬組件中的生命週期鉤子)
+    (2) React中的副作用操作:
+            發ajax請求數據獲取
+            設置訂閱/啟動定時
+            手動更改真實DOM
+    (3) 語法和說明:
+            useEffect(() => {
+                // 在此可執行任何帶副作用操作
+                return () => {// 在組件卸載前執行
+                    // 在此做一些收尾工作, 比如清除定時器/ 取消訂閱等
+                }
+            }, [stateValue])    //如果指定的是[], 回調函數只會在第一次render()後執行
+
+    (4) 可以把useEffect Hook 看作如下三個函數的組合
+            componentDidMount()
+            componentDidUpdate()
+            componentWillUnmount()
+
+
+
+
--- src/components/3_hooks/index.jsx
+++ src/components/3_hooks/index.jsx
@@ -1,45 +1,74 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 
-// export default class Demo extends React.Component {
+// 類式組件
+class Demo extends React.Component {
     
-//     state = {count:0}
+    state = {count:0}
     
-//     add = () => {
-//         this.setState(state => ({count: state.count+1}))
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <h2>當前求和為{this.state.count}</h2>
-//                 <button onClick={this.add}>點我+1</button>
-//             </div>
-//         )
-//     }
-// }
-function Demo(){
-    console.log('Demo');
-    const [count, setCount] = React.useState(0);
-    const [name, setName] = React.useState('tom');
-
-    //加的回調
-    function add(){
-        // setCount(count+1);  // 第一種寫法
-        setCount(count => count+1)
+    add = () => {
+        this.setState(state => ({count: state.count+1}))
     }
 
-    function changeName(){
-        setName('jack');
+    unmount = () =>{
+        ReactDOM.unmountComponentAtNode(document.getElementById('root'));
     }
 
-    return(
-        <div>
-            <h2>當前求和為{count}</h2>
-            <h2>我的名子是:{name}</h2>
-            <button onClick={add}>點我+1</button>
-            <button onClick={changeName}>點我改名</button>
-        </div>
-    )
+    componentDidMount(){
+        this.timer = setInterval(() =>{
+            this.setState(state => ({count: state.count+1}))
+        }, 1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timer)
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>當前求和為{this.state.count}</h2>
+                <button onClick={this.add}>點我+1</button>
+                <button onClick={this.unmount}>卸載組件</button>
+            </div>
+        )
+    }
 }
+
+
+// function Demo(){
+//     // console.log('Demo');
+//     const [count, setCount] = React.useState(0);
+
+//     React.useEffect(()=>{
+//        let timer = setInterval(()=>{
+//             setCount(count => count+1);
+//        },1000) 
+
+//        return ()=>{
+//             clearInterval(timer)    
+//        }
+//     },[])
+
+
+//     //加的回調
+//     function add(){
+//         // setCount(count+1);  // 第一種寫法
+//         setCount(count => count + 1)
+//     }
+
+//     //卸載組件的回調
+//     function unmount(){
+//         ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+//     }
+
+//     return(
+//         <div>
+//             <h2>當前求和為{count}</h2>
+//             <button onClick={add}>點我+1</button>
+//             <button onClick={unmount}>卸載組件</button>
+//         </div>
+//     )
+// }
 
 export default Demo
